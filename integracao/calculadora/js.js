ab7d64039a2243195412833eecdb78e7fc980913
malulattaa <maria.latta2@estudante.ifms.edu.br>
2025-11-16 00:45:10 -0400

    add repetição para inserir os botões // arrumar %, . e CE

--- integracao/calculadora/js.js
+++ integracao/calculadora/js.js
@@ -1,29 +1,37 @@
+const tela = document.querySelector('.tela')
+
+const button = {
+    um: '1',
+    dois: '2',
+    tres: '3',
+    quatro: '4',
+    cinco: '5',
+    seis: '6',
+    sete: '7',
+    oito: '8',
+    nove: '9',
+    zero: '0',
+    adicao: "+",
+    subtracao: "-",
+    multiplicar: "*",
+    dividir: "/"
+
+}
+for (let elemento in button){
+    document.getElementById(elemento).addEventListener('click', () => {inserir(button[elemento])})
+}
+
 function inserir(a){
-    var tela = document.querySelector('.tela').innerHTML;
-    document.querySelector('.tela').innerHTML = tela + a;
+    tela.innerHTML += a;
 }
 
 function calcular(){
-    let tela = document.querySelector('.tela')
     let expressao = tela.innerHTML
     let result = eval(expressao)
     // eval é uma função nativa que lê a string e executa oq estiver escrito dentro dela
     tela.innerHTML = result
 }
-document.getElementById('um').addEventListener('click', () => {inserir('1')})
-document.getElementById('dois').addEventListener('click', () => {inserir('2')})
-document.getElementById('tres').addEventListener('click', () => {inserir('3')})
-document.getElementById('quatro').addEventListener('click', () => {inserir('4')})
-document.getElementById('cinco').addEventListener('click', () => {inserir('5')})
-document.getElementById('seis').addEventListener('click', () => {inserir('6')})
-document.getElementById('sete').addEventListener('click', () => {inserir('7')})
-document.getElementById('oito').addEventListener('click', () => {inserir('8')})
-document.getElementById('nove').addEventListener('click', () => {inserir('9')})
-document.getElementById('zero').addEventListener('click', () => {inserir('0')})
-document.getElementById('C').addEventListener('click', () => {document.querySelector('.tela').innerHTML=''})
-document.getElementById('adicao').addEventListener('click', () => {inserir('+')})
+
+document.getElementById('C').addEventListener('click', () => {tela.innerHTML=''})
 document.getElementById('igual').addEventListener('click', () => {calcular()})
-document.getElementById('subtracao').addEventListener('click', () => {inserir('-')})
-document.getElementById('multiplicar').addEventListener('click', () => {inserir('*')})
-document.getElementById('dividir').addEventListener('click', () => {inserir('/')})
-document.getElementById('porcentagem').addEventListener('click', () => {inserir('%')})//arrumar
+
